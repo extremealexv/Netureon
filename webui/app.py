9262aa8184abb0a86735d9a75efc3f63fc9cee64
extremealexv <alexander.vasilyev83@gmail.com>
2025-08-02 21:34:20 -0400

    added review page to webui

--- webui/app.py
+++ webui/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 import os
 from dotenv import load_dotenv
@@ -33,3 +33,48 @@ def main_page():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+
+@app.route('/review')
+def review_page():
+    conn = psycopg2.connect(**DB_CONFIG)
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT device_name, mac_address, device_type, last_seen, last_ip, notes
+        FROM new_devices
+        ORDER BY last_seen DESC
+    """)
+    new_devices = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return render_template('review.html', new_devices=new_devices)
+
+
+@app.route('/add_known', methods=['POST'])
+def add_to_known():
+    selected = request.form.getlist('selected')
+
+    if not selected:
+        return redirect(url_for('review_page'))
+
+    conn = psycopg2.connect(**DB_CONFIG)
+    cur = conn.cursor()
+
+    for mac in selected:
+        cur.execute("""
+            INSERT INTO known_devices (device_name, mac_address, device_type, last_seen, last_ip, notes)
+            SELECT device_name, mac_address, device_type, last_seen, last_ip, notes
+            FROM new_devices
+            WHERE mac_address = %s
+        """, (mac,))
+        cur.execute("DELETE FROM new_devices WHERE mac_address = %s", (mac,))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return redirect(url_for('main_page'))
