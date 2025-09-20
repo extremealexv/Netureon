91007850b8a8a24f721f77f4b511d49d3864e36e
extremealexv <alexander.vasilyev83@gmail.com>
2025-09-20 15:17:53 -0400

    fixed deletion from unknown devices list

--- webui/routes/unknown.py
+++ webui/routes/unknown.py
@@ -102,23 +102,24 @@ def handle_delete_action(selected_devices):
     for mac in selected_devices:
         try:
             mac_clean = mac.strip().lower()
+            # Each query now uses named parameters
             queries = [
                 # Remove from discovery_log
                 ("""
                     DELETE FROM discovery_log 
-                    WHERE mac_address::macaddr = %s::macaddr
-                """, [mac_clean]),  # Changed tuple to list
+                    WHERE mac_address::macaddr = %(mac)s::macaddr
+                """, {'mac': mac_clean}),
                 # Remove any existing alerts
                 ("""
                     DELETE FROM alerts 
-                    WHERE device_id::macaddr = %s::macaddr
-                """, [mac_clean]),  # Changed tuple to list
+                    WHERE device_id::macaddr = %(mac)s::macaddr
+                """, {'mac': mac_clean}),
                 # Finally remove from unknown_devices
                 ("""
                     DELETE FROM unknown_devices 
-                    WHERE mac_address = %s::macaddr
+                    WHERE mac_address = %(mac)s::macaddr
                     RETURNING 1
-                """, [mac_clean])  # Changed tuple to list
+                """, {'mac': mac_clean})
             ]
             
             # Execute all queries in a transaction
